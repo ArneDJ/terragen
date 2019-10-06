e57cfa752b8b349fe005e3525b410bb03325d492
ArneDJ <ArneDeJonghe@protonmail.com>
2019-10-06 13:54:04 +0200

    added camera

---
+++ src/camera.c
@@ -0,0 +1,41 @@
+#include <SDL2/SDL.h>
+#include "gmath.h"
+#include "camera.h"
+
+struct camera init_camera(float x, float y, float z, float fov, float sensitivity)
+{
+	struct camera cam = {
+		0.0, 0.0, fov, sensitivity, 1.0,
+		{0.0, 0.0, 0.0},
+		{0.0, 1.0, 0.0},
+		{x, y, z},
+	};
+
+	return cam;
+}
+
+void update_camera(struct camera *cam, float delta)
+{
+	int x, y;
+	const Uint8 *keystates = SDL_GetKeyboardState(NULL);
+	SDL_GetRelativeMouseState(&x, &y);
+
+	cam->yaw += (float)x * cam->sensitivity * delta;
+	cam->pitch -= (float)y * cam->sensitivity * delta;
+
+	if (cam->pitch > 1.57f)	
+		cam->pitch = 1.57f;
+	if (cam->pitch < -1.57f)	
+		cam->pitch = -1.57f;
+
+	/* point the camera in a direction based on mouse input */
+	cam->center.x = cos(cam->yaw) * cos(cam->pitch);
+	cam->center.y = sin(cam->pitch);
+	cam->center.z = sin(cam->yaw) * cos(cam->pitch);		
+	cam->center = vec3_normalize(cam->center);
+
+	if(keystates[SDL_SCANCODE_W]) cam->eye = vec3_sum(cam->eye, vec3_scale(cam->speed * delta , cam->center));
+	if(keystates[SDL_SCANCODE_S]) cam->eye = vec3_sub(cam->eye, vec3_scale(cam->speed * delta, cam->center));
+	if(keystates[SDL_SCANCODE_D]) cam->eye = vec3_sum(cam->eye, vec3_scale(cam->speed * delta, vec3_crossn(cam->center, cam->up)));
+	if(keystates[SDL_SCANCODE_A]) cam->eye = vec3_sub(cam->eye, vec3_scale(cam->speed * delta, vec3_crossn(cam->center, cam->up)));
+}
--- src/main.c
+++ src/main.c
@@ -6,54 +6,10 @@
 #include "shader.h"
 #include "mesh.h"
 #include "gmath.h"
+#include "camera.h"
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 960
-#define CAMERA_SPEED 2
-
-struct camera {
-	float yaw;
-	float pitch;
-	vec3 center;
-	vec3 up;
-	vec3 eye;
-};
-
-enum {
-	FORWARD = 26,
-	BACKWARD = 22,
-	RIGHT = 7,
-	LEFT = 4,
-};
-
-static void view(struct camera *cam, float delta)
-{
-	int x, y;
-	const Uint8 *keystates = SDL_GetKeyboardState(NULL);
-	SDL_GetRelativeMouseState(&x, &y);
-
-	cam->yaw += (float)x * 0.1 * delta;
-	cam->pitch -= (float)y * 0.1 * delta;
-	cam->pitch = (cam->pitch > 1.57) ? 1.57 : (cam->pitch < -1.57) ? -1.57 : cam->pitch;
-
-	/* point the camera in a direction based on mouse input */
-	cam->center.x = cos(cam->yaw) * cos(cam->pitch);
-	cam->center.y = sin(cam->pitch);
-	cam->center.z = sin(cam->yaw) * cos(cam->pitch);		
-	cam->center = vec3_normalize(cam->center);
-
-	vec3 cross = vec3_crossn(cam->center, cam->up);
-
-	cam->center = vec3_scale(CAMERA_SPEED * delta, cam->center);
-	cross = vec3_scale(CAMERA_SPEED * delta, cross);
-
-	if(keystates != NULL) {
-	if(keystates[FORWARD]) cam->eye = vec3_sum(cam->eye, cam->center);
-	if(keystates[BACKWARD]) cam->eye = vec3_sub(cam->eye, cam->center);
-	if(keystates[RIGHT]) cam->eye = vec3_sum(cam->eye, cross);
-	if(keystates[LEFT]) cam->eye = vec3_sub(cam->eye, cross);
-	}
-}
 
 int main(int argc, char *argv[])
 {
@@ -87,12 +43,7 @@ int main(int argc, char *argv[])
 	mat4 model = IDENTITY_MATRIX;
 	glUniformMatrix4fv(glGetUniformLocation(cube_program, "model"), 1, GL_FALSE, model.f);
 
-	struct camera cam = {
-		0.0, 0.0,
-		{0.0, 0.0, 0.0},
-		{0.0, 1.0, 0.0},
-		{0.0, 0.0, 0.0},
-	};
+	struct camera cam = init_camera(0.0, 0.0, 0.0, 90.0, 0.2);
 	SDL_SetRelativeMouseMode(SDL_TRUE);
 	SDL_Event event;
 	float start, end = 0.0;
@@ -103,7 +54,7 @@ int main(int argc, char *argv[])
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		view(&cam, delta);
+		update_camera(&cam, delta);
 		mat4 view = make_view_matrix(cam.eye, cam.center, cam.up);
 
 		glUseProgram(cube_program);
